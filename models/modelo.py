4b65ab419ad8f7917b3d02d82bed1ac7bfea24dd
Rafael Felipe <rfaeldel@outlook.com>
2022-09-02 19:06:33 -0300

    Implementação de banco de dados e atualização dos métodos GET e POST

--- models/modelo.py
+++ models/modelo.py
@@ -1,10 +1,20 @@
-class HotelModel:
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision =1))
+    diaria = banco.Column(banco.Float(precision = 2))
+    cidade = banco.Column(banco.String(40))
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
        self.hotel_id = hotel_id
        self.nome = nome
        self.estrelas = estrelas
        self.diaria = diaria
-       self.cidade = cidade 
+       self.cidade = cidade
 
     def json(self):
         return{
@@ -14,3 +24,14 @@ class HotelModel:
             'diaria': self.diaria,
             'cidade': self.cidade
         }
+
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id=hotel_id).first() #SELECT * FROM hoteis WHERE hotel_id = $hotel_id limit = 1
+        if hotel:
+            return hotel
+        return False
+
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
--- resources/hotel.py
+++ resources/hotel.py
@@ -26,22 +26,6 @@ hoteis =[
     }
     
 ]
-class HotelModel:
-    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
-       self.hotel_id = hotel_id
-       self.nome = nome
-       self.estrelas = estrelas
-       self.diaria = diaria
-       self.cidade = cidade 
-
-    def json(self):
-        return{
-            'hotel_id' : self.hotel_id,
-            'nome': self.nome,
-            'estrela': self.estrelas,
-            'diaria': self.diaria,
-            'cidade': self.cidade
-        }
 
 class Hoteis(Resource):
     def get(self):
@@ -53,37 +37,33 @@ class Hotel(Resource):
         atributos.add_argument('estrelas')
         atributos.add_argument('diaria')
         atributos.add_argument('cidade')
-        
-        def find_hotel(hotel_id):
-            for hotel in hoteis:
-                if hotel['hotel_id'] == hotel_id:
-                    return hotel
-            return None
     
         def get(self, hotel_id):
-            hotel = Hotel.find_hotel(hotel_id)
+            hotel = HotelModel.find_hotel(hotel_id)
             if hotel:
-                return hotel
+                return hotel.json()   
             return{'message':'Hotel not found.'}, 404
 
         
         def post(self, hotel_id):
+            if HotelModel.find_hotel(hotel_id):
+                return{"message":"Hotel id '{}' already exists.".format(hotel_id)}, 400
             dados = Hotel.atributos.parse_args()
-            hotel_objeto = HotelModel(hotel_id, **dados)
-            novo_hotel= hotel_objeto.json()
-            hoteis.append(novo_hotel)
-            return novo_hotel, 201
-        
-
-        
+            hotel = HotelModel(hotel_id, **dados)
+            hotel.save_hotel()
+            return hotel.json()
+            
+              
         def put(self, hotel_id):
+        
             dados = Hotel.atributos.parse_args()
-            hotel_objeto = HotelModel(hotel_id, **dados)
-            novo_hotel= hotel_objeto.json()
+            novo_hotel = {'hotel_id': hotel_id, **dados}
+
             hotel = Hotel.find_hotel(hotel_id)
             if hotel:
                 hotel.update(novo_hotel)
-            return novo_hotel, 201 # update
+            hoteis.append(novo_hotel)
+            return novo_hotel, 201 # created
             
     
         def delete(self, hotel_id):
